16160b4df2e9fc73c2033c5c3917cb9b1df9860b
Caio Abreu <caio3015@hotmail.com>
2023-12-21 19:01:26 -0300

    add more init files

---
+++ src/main/java/br/com/caio/javafeatures/JavaFeaturesApplication.java
@@ -0,0 +1,13 @@
+package br.com.caio.javafeatures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFeaturesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/caio/javafeatures/JavaFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caio.javafeatures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
